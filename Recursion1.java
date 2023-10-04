4b3ba10486f0994d2ff6bdbcb16c51c4bb5ab57b
Arpita Sawant <97539573+arpitasawant@users.noreply.github.com>
2023-10-05 00:28:41 +0530

    Update Recursion1.java

--- Recursion1.java
+++ Recursion1.java
@@ -1,6 +1,6 @@
 public class Recursion1 {
     public static void printNum(int n) {
-        // print numbers from 5 to 1
+        // Print numbers from 5 to 1
         if (n == 0) {
         return;
     }
