64c8656b665f8d4ba5d197a47c7a8af3329bcd9c
Arpita <arpitasawant2002@gmail.com>
2023-12-15 19:41:50 +0530

    Updated

--- twosums.java
+++ twosums.java
@@ -1,23 +1,21 @@
-import java.util.Scanner;
-
 public class twosums{
-    public static void main(String[] args){
-       int n;
-        int[] arr=new int[5];
-        Scanner scan=new Scanner(System.in);
-     
-      System.out.println("enter elements: "); 
-      n=scan.nextInt();
-       //scan.close();
-        for(int i=0;i<n;i++){
-           arr[i]=scan.nextInt();
-            //System.out.println(n[i]);
+    public static void twosumproblem(int[] arr, int target) {
+
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                 sum = arr[i]+arr[j];
+            if(sum == target){
+                System.out.println("["+i+" "+j+"]");
+            }
+            }
+            
         }
-        System.out.println("elements are: ");
-        for(int i=1;i<n;i++){
-            System.out.println("target="+arr[i]);
-        
-        
-    } System.out.println("hello");
-   
-}}
+    }
+    public static void main(String[] args) {
+        int arr[] = {3,3};
+        twosumproblem(arr,6);
+    }
+
+    
+}
